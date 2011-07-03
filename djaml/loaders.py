df8036b0761a1bc9d5a5d47f43e0b1e70d94240d
Roman Dolgiy <roman@bravetstudio.com>
2011-07-03 21:06:53 +0300

    Changed `load_template_source` behavior
    If you will specify "profile.html" as template name,
    `djaml` template loader will look search for "profile.hamlpy"
    and "profile.haml" templates.
    
    Such behavior allows you to override templates of
    reusable apps with `djaml`.

--- djaml/loaders.py
+++ djaml/loaders.py
@@ -14,19 +14,29 @@ def get_haml_loader(loader):
 
     class Loader(baseclass):
         def load_template_source(self, template_name, *args, **kwargs):
-            if not template_name.endswith('.haml'):
-                raise TemplateDoesNotExist(template_name)
-            
-            haml_source, template_path = super(Loader, self).load_template_source(template_name, *args, **kwargs)
-            hamlParser = hamlpy.Compiler()
-            html = hamlParser.process(haml_source)
-            
-            return html, template_path
+            _name, _extension = template_name.split(".")
+
+            for extension in ["hamlpy", "haml"]:
+                try:
+                    haml_source, template_path = super(Loader, self).load_template_source(
+                        self._generate_template_name(_name, extension), *args, **kwargs
+                    )
+                except TemplateDoesNotExist:
+                    pass
+                else:
+                    hamlParser = hamlpy.Compiler()
+                    html = hamlParser.process(haml_source)
+
+                    return html, template_path
+
+            raise TemplateDoesNotExist(template_name)
 
         load_template_source.is_usable = True
+
+        def _generate_template_name(self, name, extension="hamlpy"):
+            return "%s.%s" % (name, extension)
 
     return Loader
 
 haml_loaders = dict((name, get_haml_loader(loader))
         for (name, loader) in get_django_template_loaders())
-
